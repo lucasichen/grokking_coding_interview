5d10bb31ac7b7da2b208c9aa8ebe4f97b50c2144
Lucas Chen <lucas.chen2016@gmail.com>
2022-12-28 14:26:53 -0500

    fix 107 and add 104

--- 104.maximum-depth-of-binary-tree.py
+++ 104.maximum-depth-of-binary-tree.py
@@ -1,7 +1,7 @@
 #
-# @lc app=leetcode id=107 lang=python3
+# @lc app=leetcode id=104 lang=python3
 #
-# [107] Binary Tree Level Order Traversal II
+# [104] Maximum Depth of Binary Tree
 #
 
 # @lc code=start
@@ -13,20 +13,18 @@
 #         self.right = right
 from collections import deque
 class Solution:
-    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root is None: return []
-        res = []
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None: return 0
         queue = deque()
         queue.append(root)
+        dep = 0
         while queue:
             levelSize = len(queue)
-            currentLevel = []
+            dep += 1
             for _ in range(levelSize):
                 currentNode = queue.popleft()
-                currentLevel.append(currentNode.val)
                 if currentNode.left: queue.append(currentNode.left)
                 if currentNode.right: queue.append(currentNode.right)
-            res.append(currentLevel)
-        return res[::-1]
+        return dep
 # @lc code=end
 
