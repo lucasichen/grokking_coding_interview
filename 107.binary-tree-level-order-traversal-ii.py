50f15765009c981133c478a0fa5399658d23a24e
Lucas Chen <lucas.chen2016@gmail.com>
2022-12-28 14:19:53 -0500

    add 107

---
+++ 107.binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=107 lang=python3
+#
+# [107] Binary Tree Level Order Traversal II
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None: return []
+        res = []
+        queue = deque()
+        queue.append(root)
+        while queue:
+            levelSize = len(queue)
+            currentLevel = []
+            for _ in range(levelSize):
+                currentNode = queue.popleft()
+                currentLevel.append(currentNode.val)
+                if currentNode.left: queue.append(currentNode.left)
+                if currentNode.right: queue.append(currentNode.right)
+            res.append(currentLevel)
+        return res[::-1]
+# @lc code=end
+
